c6181852077b166239c78f66b72031714b1997f6
Chris Gitmed <cgitmed@gmail.com>
2021-01-24 23:37:46 +0000

    Completed Shuffle the Array

---
+++ leetcode/shuffle-the-array.js
@@ -0,0 +1,18 @@
+/**
+ * Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+ * Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+ */
+
+/**
+* @param {number[]} nums
+* @param {number} n
+* @return {number[]}
+*/
+const shuffle = function(nums, n) {
+  const returnedArr = [];
+  for (let i = 0; i < n; i++) {
+    returnedArr.push(nums[i]);
+    returnedArr.push(nums[i + n])
+  }
+  return returnedArr;
+};
