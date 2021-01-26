7a87d2bee0c29e679949a0bf8d4187cb273acd23
Chris Gitmed <cgitmed@gmail.com>
2021-01-26 22:59:54 +0000

    Finished add.js

---
+++ codesignal/add.js
@@ -0,0 +1,11 @@
+/**
+ *  Write a function that returns the sum of two numbers.
+ *
+ *  @param: {number} param1
+ *  @param: {number} param2
+ *  @return: {number}
+ */
+
+function add(param1, param2) {
+  return param1 + param2;
+}
