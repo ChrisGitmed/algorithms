c4f8f6e409fc953138719b57273f73adc8d95cfa
Chris Gitmed <cgitmed@gmail.com>
2021-02-09 07:11:29 +0000

    Added unit tests for century-from-year.js

---
+++ codesignal/intro/century-from-year.test.js
@@ -0,0 +1,14 @@
+const { test , expect } = require('@jest/globals');
+const centuryFromYear = require('./century-from-year');
+
+test('Expect return of centuryFromYear(1905) to be 20.', () => {
+  expect(centuryFromYear(1905)).toBe(20);
+});
+
+test('Expect return of centuryFromYear(1700) to be 17.', () => {
+  expect(centuryFromYear(1700)).toBe(17);
+});
+
+test('Expect return of centuryFromYear(45) to be 1.', () => {
+  expect(centuryFromYear(45)).toBe(1);
+});
