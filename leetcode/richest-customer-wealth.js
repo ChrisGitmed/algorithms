beda64a6b0c6ce37cd3e88ff5f4f2120f46ed3d2
Chris Gitmed <cgitmed@gmail.com>
2021-02-14 05:18:10 +0000

    Fixed weird error in first comment (irregular whitespace?).

--- leetcode/richest-customer-wealth.js
+++ leetcode/richest-customer-wealth.js
@@ -1,6 +1,6 @@
 /**
  * You are given an m x n integer grid accounts where accounts[i][j] is the
- * amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that
+ * amount of money the ith customer has in the jth bank. Return the wealth that
  * the richest customer has.
  *
  * A customer's wealth is the amount of money they have in all their bank
