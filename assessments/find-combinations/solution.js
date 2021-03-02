791d476c725405e977e1ea341c9c1729159ac871
Chris Gitmed <cgitmed@gmail.com>
2021-03-01 17:17:18 -0800

    Create some logic which places the beginning and ending hashes of the word into an array.

--- assessments/find-combinations/solution.js
+++ assessments/find-combinations/solution.js
@@ -16,5 +16,34 @@ export function findCombinations(phoneNumber, word) {
     8: ['t', 'u', 'v'],
     9: ['w', 'x', 'y', 'z']
   }
+  console.log(`phoneNumber: ${phoneNumber}, word: ${word}`);
+  // Find the word, mark the starting and ending indexes
+  let startIndex, endIndex;
+  for (let i = 0; i < phoneNumber.length && !endIndex; i++) {
+    let hash = digitToLetterHash[phoneNumber[i]];
+    if (hash.includes(word[0]) && !startIndex){
+      startIndex = i;
+      for (let x = 0; x < word.length; x++) {
+        hash = digitToLetterHash[phoneNumber[x + i]];
+        if (hash.includes(word[word.length - 1]) && x === word.length - 1) {
+          endIndex = i + x;
+        }
+      }
+    }
+  }
+  console.log(`startIndex: ${startIndex}, endIndex: ${endIndex}`);
+
+  //Get the values into two matrixes.
+  const preWordArray = [];
+  const postWordArray = [];
+  for (let i = 0; i < phoneNumber.length; i++) {
+    let hash = digitToLetterHash[phoneNumber[i]];
+    if (i < startIndex) {
+      preWordArray.push(hash);
+    } else if (i > endIndex) {
+      postWordArray.push(hash);
+    }
+  }
+  console.log(`preWordArray: ${preWordArray}, postWordArray: ${postWordArray}`);
 	return []
 }
