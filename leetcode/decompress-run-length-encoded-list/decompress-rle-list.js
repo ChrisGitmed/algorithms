db909fb0ed885157560e8ab8049f4d5c8c07bf7c
Chris Gitmed <cgitmed@gmail.com>
2021-03-04 21:45:48 -0800

    Completed 'Decompress Run-Length Encoded List'

---
+++ leetcode/decompress-run-length-encoded-list/decompress-rle-list.js
@@ -0,0 +1,35 @@
+/**
+ * We are given a list nums of integers representing 
+ * a list compressed with run-length encoding.
+ * 
+ * Consider each adjacent pair of elements 
+ * [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  
+ * For each such pair, there are freq elements with 
+ * value val concatenated in a sublist. Concatenate all 
+ * the sublists from left to right to generate the 
+ * decompressed list.
+ * 
+ * Return the decompressed list.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const decompressRLElist = (nums) => {
+    const returnedArr = [];
+    for (let i = 0; i < nums.length; i += 2) {
+        for (let j = 0; j < nums[i]; j++) {
+            returnedArr.push(nums[i + 1])
+        }
+    }
+    return returnedArr;
+};
+
+//
+// Initialize array
+// Loop through entire nums array, skipping through every other element
+//      get the value of nums[i + 1]
+//      Loop nums[i] (frequency)
+//          push the value of nums[i + 1] to the array
+// return array
