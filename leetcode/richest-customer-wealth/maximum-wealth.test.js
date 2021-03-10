71688b2a612314231710921940449ae1df432c02
Chris Gitmed <cgitmed@gmail.com>
2021-03-09 20:18:23 -0800

    Migrated to folder.

--- leetcode/richest-customer-wealth/maximum-wealth.test.js
+++ leetcode/richest-customer-wealth/maximum-wealth.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@jest/globals');
-const maximumWealth = require('./richest-customer-wealth');
+const maximumWealth = require('./maximum-wealth');
 
 test('Expect maximumWealth([[1, 2, 3], [3, 2, 1]]) to return 6', () => {
   expect(maximumWealth([[1, 2, 3], [3, 2, 1]])).toBe(6);
