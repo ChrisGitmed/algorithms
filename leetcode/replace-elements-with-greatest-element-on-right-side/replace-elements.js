516fb70ff6de3e96f528f14ace2fd849ce7c7f17
Chris Gitmed <cgitmed@gmail.com>
2021-03-31 22:12:46 -0700

    Added extra info for replaceElement

--- leetcode/replace-elements-with-greatest-element-on-right-side/replace-elements.js
+++ leetcode/replace-elements-with-greatest-element-on-right-side/replace-elements.js
@@ -1,3 +1,14 @@
+/**
+ * Given an array arr, replace every element in that array with the greatest element among 
+ * the elements to its right, and replace the last element with -1. 
+ * 
+ * After doing so, return the array.
+ */
+
+/**
+ * @param {number[]} arr 
+ * @returns {number[]}
+ */
 const replaceElements = function(arr) {
     const returnedArr = [];
     for (let i = 0; i < arr.length; i++) {
