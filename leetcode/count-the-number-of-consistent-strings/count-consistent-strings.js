5af19b8448db09a4b4ccdac6a5772dc4e511b5f4
Chris Gitmed <cgitmed@gmail.com>
2021-12-26 09:07:25 -0700

    Updated countConsistentStrings

--- leetcode/count-the-number-of-consistent-strings/count-consistent-strings.js
+++ leetcode/count-the-number-of-consistent-strings/count-consistent-strings.js
@@ -1,9 +1,9 @@
 /**
- * You are given a string allowed consisting of distinct characters 
- * and an array of strings words. A string is consistent if all 
+ * You are given a string 'allowed' consisting of distinct characters 
+ * and an array of strings 'words'. A string is consistent if all 
  * characters in the string appear in the string allowed. 
  * 
- * Return the number of consistent strings in the array words.
+ * Return the number of consistent strings in the array 'words'.
  */
 
 /**
@@ -11,24 +11,14 @@
  * @param {string[]} words
  * @return {number}
  */
- var countConsistentStrings = function(allowed, words) {
-    let counter = 0;
-    for (let i = 0; i < words.length; i++) {
-        for (let x = 0; x < words[i].length; x++) {
-            if (!allowed.includes(words[i][x])) break;
-            if (x === words[i].length - 1) counter++;
-        }
-    }
-    return counter;
+ const countConsistentStrings = (allowed, words) => {
+    let count = 0;
+    for (const word of words) {
+        let flag = true;
+        for (const letter of word) if (!allowed.includes(letter)) flag = false;
+        if (flag) count++;
+    };
+    return count;
 };
-
-/**
- * Initialize a variable for counter
- * Loop over every word in the array
- *      Loop over every char in the word
- *          If the char is NOT included in allowed, break the loop
- *          If you make it through the last char, increase counter
- * Return the counter
- */
 
 module.exports = countConsistentStrings;
