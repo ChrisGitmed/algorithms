b01220befc8393b5ce56ea35dcfadcd46945e2b7
Chris Gitmed <cgitmed@gmail.com>
2021-12-26 14:05:01 -0800

    Updated kidsWithCandies

--- leetcode/kids-with-the-greatest-number-of-candies/kids-with-candies.js
+++ leetcode/kids-with-the-greatest-number-of-candies/kids-with-candies.js
@@ -13,32 +13,9 @@
  * @param: {number} extraCandies
  * @return: {boolean[]}
  */
-function kidsWithCandies(candies, extraCandies) {
-  let max = -Infinity;
-  for (let i = 0; i < candies.length; i++) {
-    if (candies[i] > max) { max = candies[i] }
-  }
-  const boolArray = [];
-  for (let i = 0; i < candies.length; i++) {
-    if (candies[i] + extraCandies >= max) { boolArray.push(true) }
-    else { boolArray.push(false) }
-  }
-  return boolArray;
-}
+module.exports = kidsWithCandies = (candies, extraCandies) => {
+  const max = Math.max(...candies);
 
-/**
- * Create a max variable
- * for loop through entire array
- *     check if array[i] is greater than max
- *         if yes, max = array[i]
- *
- * Create new array for booleans
- * for loop through array again
- *     check if array[i] PLUS extraCandies is greater than or equal to max.
- *         if yes, push true to the array
- *         if false: push false to the array.
- *
- * return the array
- */
+  return candies.map(num => num + extraCandies >= max ? true : false);
+};
 
-module.exports = kidsWithCandies;
