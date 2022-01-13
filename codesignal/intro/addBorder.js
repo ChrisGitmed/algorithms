e46b445680e810e439f523545bae5f61467bb165
Chris Gitmed <cgitmed@gmail.com>
2022-01-12 20:36:42 -0800

    Completed addBorder.js

---
+++ codesignal/intro/addBorder.js
@@ -0,0 +1,24 @@
+/**
+ * Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+ * 
+ * Example:
+ * picture = ["abc",
+ *            "ded"]
+ * 
+ * solution(picture) = ["*****",
+ *                      "*abc*",
+ *                      "*ded*",
+ *                      "*****"]
+ * 
+ * @param {Array[]} picture 
+ * @returns {Array[]}
+ */
+const solution = picture => {
+  const width = picture[0].length + 2;
+
+  return [
+      '*'.repeat(width),
+      ...picture.map( row => `*${row}*`),
+      '*'.repeat(width)
+  ];
+};
