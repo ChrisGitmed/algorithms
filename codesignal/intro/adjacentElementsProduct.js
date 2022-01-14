cc18739b4bff883c8cc4ab52ae02c14ba28609d3
Chris Gitmed <cgitmed@gmail.com>
2022-01-13 17:59:45 -0800

    Cleaned adjacentElementsProduct.js

---
+++ codesignal/intro/adjacentElementsProduct.js
@@ -0,0 +1,17 @@
+/**
+ * Given an array of integers, find the pair of adjacent elements
+ * that has the largest product and return that product.
+ *
+ * @param: { Number[] } inputArray
+ * @return: { Number } largestProduct
+ */
+const adjacentElementsProduct = inputArray => {
+  let max = -Infinity;
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    const product = inputArray[i] * inputArray[i + 1];
+    if (product > max) max = product;
+  };
+  return max;
+};
+
+ module.exports = adjacentElementsProduct;
