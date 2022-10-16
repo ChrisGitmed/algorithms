e062c9f5963d92769fd17919a5630a1256fbac92
Chris Gitmed <chris@Chriss-MacBook-Pro.local>
2022-10-16 14:01:20 -0700

    Completed two-sum

--- leetcode/easy/solutions/two-sum.js
+++ leetcode/easy/solutions/two-sum.js
@@ -1,8 +1,22 @@
-/**
+/** 
+ * Given an array of integers nums and an integer target, return indices
+ * of the two numbers such that they add up to target.
+ * 
+ * You may assume that each input would have exactly one solution, and
+ * you may not use the same element twice.
+ * 
+ * You can return the answer in any order.
+ * 
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 const twoSum = (nums, target) => {
-  // Getting a commit for the day
+  for (let i = 0; i < nums.length; i++) {
+    for (let x = i + 1; x < nums.length; x++) {
+      if (nums[i] + nums[x] === target) return [i, x];
+    }
+  }
 };
+
+module.exports = { twoSum };
