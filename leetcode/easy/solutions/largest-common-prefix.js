8eac090a8a4376fc6743b317397f60309630c682
Chris Gitmed <chris@ip-192-168-0-24.us-west-1.compute.internal>
2022-10-18 22:49:21 -0700

    Completed largest-common-prefix

---
+++ leetcode/easy/solutions/largest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ * 
+ * @param {string[]} strs
+ * @return {string}
+ */
+ const longestCommonPrefix = (strs) => {
+  if (!strs.length) return '';
+
+  for (let i = 0; i <= strs[0].length; i++) {
+    if (!strs.every((string) => string[i] === strs[0][i])) return strs[0].slice(0, i);
+  }
+
+  return strs[0];
+};
+
+module.exports = { longestCommonPrefix };
---
+++ leetcode/easy/tests/largest-common-prefix.test.js
@@ -0,0 +1,12 @@
+const { test, expect } = require('@jest/globals');
+const { longestCommonPrefix } = require('../solutions/largest-common-prefix');
+
+test('Should return the longest common prefix.', () => {
+  const strs = ['flower', 'flow', 'flight']
+  expect(longestCommonPrefix(strs)).toBe('fl');
+});
+
+test('Should return the longest common prefix.', () => {
+  const strs = ['dog', 'racecar', 'car']
+  expect(longestCommonPrefix(strs)).toBe('');
+});
