b32a712fc202650a95b586f9dea132ca46969992
Chris Gitmed <chris@Chriss-MBP.home>
2022-12-26 19:21:45 -0700

    Completed arrayMaximalAdjacentDifference

---
+++ codesignal/intro/solutions/20-array-maximal-difference.js
@@ -0,0 +1,14 @@
+/**
+ * Given an array of integers, find the maximal absolute difference between
+ * any two of its adjacent elements.
+ */
+const solution = (input) => {
+  let maxDiff = 0;
+  for (let idx = 1; idx < input.length; idx += 1) {
+    const diff = Math.abs(input[idx] - input[idx - 1]);
+    if (diff > maxDiff) maxDiff = diff;
+  }
+  return maxDiff;
+}
+
+module.exports = { solution };
