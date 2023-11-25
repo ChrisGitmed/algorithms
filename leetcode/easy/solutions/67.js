54d4b5038927564ca5f042d1476c97186eb91396
Chris Gitmed <cgitmed@gmail.com>
2023-11-24 23:13:44 -0700

    Completed problem 67

---
+++ leetcode/easy/solutions/67.js
@@ -0,0 +1,10 @@
+/**
+ * Given two binary strings a and b, return their sum as a binary string.
+ * 
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+const solution = (a, b) => (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2);
+
+export { solution };
