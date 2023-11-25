df035d3240720980c63bcd7e882a16bfbff3238f
Chris Gitmed <cgitmed@gmail.com>
2023-11-24 22:50:03 -0700

    Completed problem 27

---
+++ codesignal/intro/solutions/27.js
@@ -0,0 +1,12 @@
+/**
+ * Correct variable names consist only of English letters, digits
+ * and underscores and they can't start with a digit.
+ *
+ * Check if the given string is a correct variable name.
+ * 
+ * @param {*} name 
+ * @returns {Boolean}
+ */
+const solution = (name) => /^[a-z_]\w*$/i.test(name);
+
+export { solution }
