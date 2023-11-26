850c14b79fcabe500a917c4cd04713cfb69d3cca
Chris Gitmed <cgitmed@gmail.com>
2023-11-25 18:02:51 -0800

    Completed problem 69

---
+++ leetcode/easy/solutions/69.js
@@ -0,0 +1,19 @@
+/**
+ * Given a non-negative integer x, return the square root of x rounded
+ * down to the nearest integer. The returned integer should be
+ * non-negative as well.
+ *
+ * You must not use any built-in exponent function or operator.
+ *
+ * @param {number} x
+ * @return {number}
+ */
+const solution = (x) => {
+  let square = 0;
+  while(true) {
+    if (square * square > x) return square - 1;
+    square++;
+  }
+};
+
+export { solution };
